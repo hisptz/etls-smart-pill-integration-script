83a61cda037f69f9a8916c506f057a3b3fc877a2
megamind <echingalo@gmail.com>
2023-11-02 17:57:15 +0300

    feat: added method for generating events payload for migration

--- src/services/integration.service.ts
+++ src/services/integration.service.ts
@@ -1,14 +1,36 @@
-import { Duration, Episode } from "../types";
+import {
+  AdherenceMapping,
+  DHIS2DataValue,
+  DHIS2Event,
+  Duration,
+  Episode,
+} from "../types";
 import { DateTime } from "luxon";
-import { map, head, chunk, find, forEach, filter } from "lodash";
+import {
+  map,
+  head,
+  chunk,
+  find,
+  forEach,
+  filter,
+  groupBy,
+  orderBy,
+  reduce,
+  last,
+} from "lodash";
 
 import logger from "../logging";
 import {
   getAssignedDevices,
   getProgramMapping,
 } from "../helpers/dhis2-api.helpers";
-import { getDevicesWisepillEpisodes } from "../helpers/wise-pill-api.helpers";
+import {
+  generateDataValuesFromAdherenceMapping,
+  getDevicesWisepillEpisodes,
+  getSanitizedAdherence,
+} from "../helpers/wise-pill-api.helpers";
 import dhis2Client from "../clients/dhis2";
+import { uid } from "@hisptz/dhis2-utils";
 
 function getEventDuration(startDate?: string, endDate?: string): string {
   if (!startDate && !endDate) {
@@ -39,10 +61,7 @@ export async function startIntegrationProcess({
   await getDhis2TrackedEntityInstancesWithEvents({ startDate, endDate });
 }
 
-async function getDhis2TrackedEntityInstancesWithEvents({
-  startDate,
-  endDate,
-}: Duration) {
+async function getDhis2TrackedEntityInstancesWithEvents(duration: Duration) {
   logger.info("Fetching DHIS2 program mappings.");
   const { program, programStage, attributes } = await getProgramMapping();
   if (!program || !programStage || !attributes) {
@@ -59,12 +78,14 @@ async function getDhis2TrackedEntityInstancesWithEvents({
     deviceImeis,
     attributes
   );
+
+  const { startDate, endDate } = duration;
   const events = await getDhis2Events(programStage, { startDate, endDate });
 
   logger.info("Organizing DHIS2 tracked entities and events");
   trackedEntityInstances = map(
     trackedEntityInstances,
-    ({ trackedEntityInstance, imei }) => {
+    ({ trackedEntityInstance, imei, orgUnit }) => {
       const teiEvents = filter(
         events,
         ({ trackedEntityInstance: tei }) => trackedEntityInstance === tei
@@ -72,6 +93,7 @@ async function getDhis2TrackedEntityInstancesWithEvents({
       return {
         trackedEntityInstance,
         imei,
+        orgUnit,
         events: teiEvents,
       };
     }
@@ -80,23 +102,21 @@ async function getDhis2TrackedEntityInstancesWithEvents({
   logger.info("Fetching Adherence episodes from Wisepill.");
   const episodes = await getDevicesWisepillEpisodes(deviceImeis);
 
-  const eventPayloads = generateEventPayload(episodes, trackedEntityInstances);
+  const eventPayloads = generateEventPayload(
+    episodes,
+    trackedEntityInstances,
+    program,
+    programStage,
+    {
+      startDate,
+      endDate,
+    }
+  );
 
-  await uploadDhis2Events(eventPayloads);
+  console.log(JSON.stringify(eventPayloads));
+
+  //  await uploadDhis2Events(eventPayloads);
 }
-
-//TODO generate event payloads
-
-function generateEventPayload(
-  episodes: Episode[],
-  trackedEntityInstances: { [key: string]: any }
-): any[] {
-  console.log(JSON.stringify({ trackedEntityInstances, episodes }));
-  return [];
-}
-
-//  TODO import the events
-async function uploadDhis2Events(eventPayloads: any): Promise<void> {}
 
 async function getDhis2TrackedEntityInstances(
   program: string,
@@ -113,7 +133,7 @@ async function getDhis2TrackedEntityInstances(
   let page = 1;
   for (const devices of chunkedDevices) {
     try {
-      const url = `trackedEntityInstances.json?fields=attributes,trackedEntityInstance&ouMode=ALL&filter=${imeiAttribute}:in:${devices.join(
+      const url = `trackedEntityInstances.json?fields=attributes,orgUnit,trackedEntityInstance&ouMode=ALL&filter=${imeiAttribute}:in:${devices.join(
         ";"
       )}&program=${program}&paging=false`;
 
@@ -122,7 +142,7 @@ async function getDhis2TrackedEntityInstances(
         const { trackedEntityInstances } = data;
         forEach(
           trackedEntityInstances,
-          ({ attributes, trackedEntityInstance }) => {
+          ({ attributes, trackedEntityInstance, orgUnit }) => {
             const { value: imei } = find(
               attributes,
               ({ attribute }) => attribute === imeiAttribute
@@ -130,6 +150,7 @@ async function getDhis2TrackedEntityInstances(
             sanitizedTrackedEntityInstances.push({
               imei,
               trackedEntityInstance,
+              orgUnit,
             });
           }
         );
@@ -206,4 +227,171 @@ async function getRootOrganisationUnit(): Promise<string> {
     throw new Error(e.toString());
   }
   return rootOuId;
+}
+
+function generateEventPayload(
+  episodes: Episode[],
+  trackedEntityInstances: Array<{ [key: string]: any }>,
+  program: string,
+  programStage: string,
+  duration: Duration
+): any[] {
+  logger.info("Preparing the events payloads for migrating to DHIS2");
+
+  const eventPayloads: DHIS2Event[] = [];
+  const { startDate, endDate } = duration;
+  const defaultStartDate = "1970-01-01";
+  const groupedEpisodes = groupBy(episodes, "imei");
+
+  for (const {
+    imei,
+    trackedEntityInstance,
+    orgUnit,
+    events,
+  } of trackedEntityInstances) {
+    const teiEpisodes = groupedEpisodes[imei];
+
+    logger.info(
+      `Evaluating DHIS2 event payloads for tracked entity instance ${trackedEntityInstance} assigned to device ${imei}`
+    );
+    if (teiEpisodes && teiEpisodes.length) {
+      const cummulativeEpisodes = getCummulatedEpsodes(teiEpisodes);
+      const { adherenceString, lastSeen, batteryLevel, deviceStatus, imei } =
+        cummulativeEpisodes;
+
+      const adherences = (adherenceString ?? "").split(",");
+
+      // if not range is specified
+      if (!startDate && !endDate) {
+        const lastSeenDate = DateTime.fromSQL(lastSeen);
+        const now = DateTime.now();
+
+        // if the last seen for the episode is the current day
+        if ((now.diff(lastSeenDate, ["days"]).toObject().days ?? 0) < 1) {
+          const episodeAdherence: AdherenceMapping = {
+            adherence: last(adherences) ?? "0",
+            date: lastSeen,
+          };
+          const dataValues = generateDataValuesFromAdherenceMapping(
+            episodeAdherence,
+            batteryLevel,
+            deviceStatus
+          );
+          const teiEvent: DHIS2Event = getDHIS2EventPayload(
+            program,
+            programStage,
+            trackedEntityInstance,
+            orgUnit,
+            lastSeen,
+            events,
+            dataValues
+          );
+
+          eventPayloads.push(teiEvent);
+        } else {
+          logger.warn(
+            `Episode for ${imei} can not be saved to DHIS2 on ${now}, since the device was last seen at ${lastSeen}`
+          );
+        }
+      } else {
+        // if there is some range specified
+        const episodeAdherences = getSanitizedAdherence(adherences, lastSeen);
+
+        // evaluation of the range for running the script
+        const evaluationStartDate = startDate
+          ? DateTime.fromISO(startDate)
+          : DateTime.fromISO(defaultStartDate);
+        const evaluationEndDate = endDate
+          ? DateTime.fromISO(endDate)
+          : DateTime.now();
+
+        for (const { adherence, date } of episodeAdherences) {
+          const adherenceDate = DateTime.fromISO(date);
+
+          // If the adherence date is within the range for running the script
+          if (
+            evaluationStartDate <= adherenceDate &&
+            adherenceDate <= evaluationEndDate
+          ) {
+            const dataValues = generateDataValuesFromAdherenceMapping({
+              adherence,
+              date,
+            });
+            const adherenceEvent = getDHIS2EventPayload(
+              program,
+              programStage,
+              trackedEntityInstance,
+              orgUnit,
+              date,
+              events,
+              dataValues
+            );
+            eventPayloads.push(adherenceEvent);
+          }
+        }
+      }
+    } else {
+      logger.warn(
+        `Tracked entity instance linked to device with IMEI ${imei} has not wisepill episodes`
+      );
+    }
+  }
+
+  return eventPayloads;
+}
+
+function getDHIS2EventPayload(
+  program: string,
+  programStage: string,
+  trackedEntityInstance: string,
+  orgUnit: string,
+  eventDate: string,
+  events: any[],
+  dataValues: DHIS2DataValue[]
+): DHIS2Event {
+  const sanitizedEventDate = DateTime.fromISO(eventDate).toFormat("yyyy-MM-dd");
+  const existingEvent: any =
+    events && events.length
+      ? head(filter(events, (eventDate) => eventDate == sanitizedEventDate))
+      : null;
+  const eventId = existingEvent ? existingEvent["event"] ?? uid() : uid();
+
+  return {
+    event: eventId,
+    dataValues,
+    trackedEntityInstance,
+    orgUnit,
+    program,
+    programStage,
+    eventDate: sanitizedEventDate,
+    status: "COMPLITED",
+  };
+}
+
+function getCummulatedEpsodes(episodes: Episode[]): Episode {
+  logger.info("Accumulating the episodes from the same devices");
+  const orderedEpisodes = orderBy(episodes, ["lastSeen"], ["asc"]);
+  return reduce(orderedEpisodes, (cummulatedEpisode, episode) => {
+    return {
+      ...cummulatedEpisode,
+      imei: cummulatedEpisode?.imei ?? episode.imei,
+      episodeStartDate:
+        cummulatedEpisode?.episodeStartDate ?? episode.episodeStartDate,
+      lastSeen: episode.lastSeen,
+      batteryLevel: episode.batteryLevel,
+      deviceStatus: episode.deviceStatus,
+      adherenceString: cummulatedEpisode
+        ? cummulatedEpisode.adherenceString
+        : "" + episode.adherenceString,
+    };
+  }) as Episode;
+}
+
+//  TODO import the events
+async function uploadDhis2Events(eventPayloads: any): Promise<void> {
+  try {
+    const url = `events?strategy=CREATE_AND_UPDATE`;
+    const response = dhis2Client.post(url, { events: eventPayloads });
+    console.log(JSON.stringify({ response }));
+  } catch (error: any) {}
 }
