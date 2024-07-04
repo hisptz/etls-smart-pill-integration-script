5051f2ea47d0d0ab4f673212ccc2b91efcb94de6
ericchingalo <echingalo@gmail.com>
2024-07-04 11:59:40 +0300

    refactor: updated the packagign script
    added the proper naming of files

--- package.sh
+++ package.sh
@@ -3,9 +3,10 @@
 cp .env.example build/script/
 cp build-readme.md build/script/README.md
 cp package.json build/script/package.json
-cp start-server.sh build/script/start-server.sh
+cp start-api-server.sh build/script/start-api-server.sh
 cp start-interval-integration.sh build/script/start-interval-integration.sh
 cp start-auto-integration.sh build/script/start-auto-integration.sh
+cp .env.example build/scripts/.env.example
 
 PKG_VERSION=$(node -p "require('./package.json').version")
 PKG_NAME=$(node -p "require('./package.json').name")
